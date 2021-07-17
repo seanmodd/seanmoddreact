0b575f74c63ce4bc858042f99070869c034a44d8
Sean <sean@vestaridefair.com>
2021-07-16 19:47:57 -0700

    ok styles global made

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { GlobalStyle } from './GlobalStyle';
 
 function App() {
   return (
-    <div className="App">
-      Start here.
-    </div>
+    <>
+      <div className="App">
+        Start here.
+        <GlobalStyle />
+      </div>
+    </>
   );
 }
 
